5f007743be901885c92ed45f1339013e725b3b12
Paras Khandelwal <khandelwalparas1699@gmail.com>
2024-08-05 13:07:16 +0530

    Add files via upload

---
+++ qrcode_gen.py
@@ -0,0 +1,10 @@
+import qrcode
+qr = qrcode.QRCode(version=1,
+                 error_correction=qrcode.constants.ERROR_CORRECT_H,
+                 box_size=10, border=4)
+print("Give a link for qr code:")
+a = input("")
+qr.add_data(a)
+qr.make(fit=True)
+img = qr.make_image(fill_color="red", back_color="black")
+img.save("d.png")
